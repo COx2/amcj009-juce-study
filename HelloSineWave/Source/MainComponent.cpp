da748227ca1e8cfd55712c7723b3fc1974da9ee7
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2020-11-12 17:23:31 +0900

    Implement wave draw visualiser.

--- HelloSineWave/Source/MainComponent.cpp
+++ HelloSineWave/Source/MainComponent.cpp
@@ -4,6 +4,7 @@
 MainComponent::MainComponent()
     : gainSlider(std::make_unique<juce::Slider>(juce::Slider::RotaryHorizontalVerticalDrag, juce::Slider::TextBoxBelow))
     , pitchSlider(std::make_unique<juce::Slider>(juce::Slider::RotaryHorizontalVerticalDrag, juce::Slider::TextBoxBelow))
+    , waveSampleCollector(waveDrawBuffer)
 {
     // Make sure you set the size of the component after
     // you add any child components.
@@ -23,10 +24,14 @@ MainComponent::MainComponent()
     }
 
     gainSlider->setRange(0.0, 1.0);
+    gainSlider->setValue(0.5f);
     addAndMakeVisible(gainSlider.get());
     
     pitchSlider->setRange(0.1, 10.0);
+    pitchSlider->setValue(1.0f);
     addAndMakeVisible(pitchSlider.get());
+
+    startTimerHz(30);
 }
 
 MainComponent::~MainComponent()
@@ -73,6 +78,8 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
         }
     }
     lastPhase = phase;
+
+    waveSampleCollector.process(buffer->getReadPointer(0), buffer->getNumSamples());
 }
 
 void MainComponent::releaseResources()
@@ -84,12 +91,47 @@ void MainComponent::releaseResources()
 }
 
 //==============================================================================
+void drawWaveShape(juce::Graphics& g, const juce::Rectangle<float>& drawArea, const float* plotData, const int numSamples)
+{
+    const auto w = drawArea.getWidth();
+    const auto h = drawArea.getHeight();
+
+    juce::Path wavePath;
+    const float x0 = drawArea.getX();
+    const float cloped_sample0 = juce::jmax<float>(-1.0f, juce::jmin<float>(1.0f, plotData[0]));
+    const float y0 = juce::jmap<float>(cloped_sample0, -1.0f, 1.0f, drawArea.getBottom(), drawArea.getY());
+    wavePath.startNewSubPath(x0, y0);
+
+    for (int i = 1; i < numSamples; ++i)
+    {
+        const float x = juce::jmap<float>(i, 0, numSamples, x0, x0 + w);
+        const float cloped_sample = juce::jmax<float>(-1.0f, juce::jmin<float>(1.0f, plotData[i]));
+        const float y = juce::jmap<float>(cloped_sample, -1.0f, 1.0f, drawArea.getBottom(), drawArea.getY());
+        wavePath.lineTo(x, y);
+    }
+    g.setColour(juce::Colours::cyan);
+    g.strokePath(wavePath, juce::PathStrokeType(2.0f));
+}
+
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    // You can add your drawing code here!
+    auto bounds = getLocalBounds();
+
+    juce::Rectangle<float> textArea = { bounds.getWidth() * 0.1f, bounds.getHeight() * 0.05f, bounds.getWidth() * 0.8f, bounds.getHeight() * 0.1f };
+    g.setColour(juce::Colours::white);
+    g.setFont(24.0f);
+    g.drawFittedText("Hello, sine wave", textArea.toNearestInt(), juce::Justification::centred, 1);
+
+    juce::Rectangle<float> drawArea = { bounds.getWidth() * 0.1f, bounds.getHeight() * 0.6f, bounds.getWidth() * 0.8f, bounds.getHeight() * 0.3f };
+    g.setColour(juce::Colours::darkgrey);
+    g.fillRect(drawArea);
+
+    // Draw wave shape
+    juce::AudioBuffer<float> samplesToDraw(1, waveDrawBuffer.getBufferSize());
+    waveDrawBuffer.pop(samplesToDraw.getWritePointer(0), samplesToDraw.getNumSamples());
+    drawWaveShape(g, drawArea, samplesToDraw.getWritePointer(0), samplesToDraw.getNumSamples());
 }
 
 void MainComponent::resized()
@@ -100,9 +142,14 @@ void MainComponent::resized()
     const int slider_h = bounds.getHeight() * 0.3f;
 
     gainSlider->setSize(slider_w, slider_h);
-    gainSlider->setCentrePosition(bounds.getWidth() * 0.25f, bounds.getHeight() * 0.5f);
+    gainSlider->setCentrePosition(bounds.getWidth() * 0.25f, bounds.getHeight() * 0.3f);
 
     pitchSlider->setSize(slider_w, slider_h);
-    pitchSlider->setCentrePosition(bounds.getWidth() * 0.75f, bounds.getHeight() * 0.5f);
+    pitchSlider->setCentrePosition(bounds.getWidth() * 0.75f, bounds.getHeight() * 0.3f);
+}
+
+void MainComponent::timerCallback()
+{
+    repaint();
 }
 
--- HelloSineWave/Source/MainComponent.h
+++ HelloSineWave/Source/MainComponent.h
@@ -3,11 +3,91 @@
 #include <JuceHeader.h>
 
 //==============================================================================
+class WaveDrawBuffer
+{
+public:
+    void push(const float* dataToPush, int numSamples)
+    {
+        jassert(numSamples <= bufferToDraw.getNumSamples());
+
+        int start1, size1, start2, size2;
+
+        abstractFifo.prepareToWrite(1, start1, size1, start2, size2);
+
+        if (size1 > 0)
+            juce::FloatVectorOperations::copy(bufferToDraw.getWritePointer(start1), dataToPush, numSamples);
+
+        abstractFifo.finishedWrite(size1);
+    }
+
+    void pop(float* outputBuffer, int numSamples)
+    {
+        jassert(numSamples <= bufferToDraw.getNumSamples());
+
+        int start1, size1, start2, size2;
+        abstractFifo.prepareToRead(1, start1, size1, start2, size2);
+
+        if (size1 > 0)
+        {
+            juce::FloatVectorOperations::copy(outputBuffer, bufferToDraw.getReadPointer(start1), numSamples);
+            lastReadPointer = start1;
+        }
+        else
+            juce::FloatVectorOperations::copy(outputBuffer, bufferToDraw.getReadPointer(lastReadPointer), numSamples);
+
+        abstractFifo.finishedRead(size1);
+
+
+    }
+
+    int getBufferSize() const
+    {
+        return bufferToDraw.getNumSamples();
+    }
+
+private:
+    const int numBuffers{ 5 };
+    const int bufferSize{ 1024 };
+    int lastReadPointer{ 0 };
+    juce::AbstractFifo abstractFifo{ numBuffers };
+    juce::AudioBuffer<float> bufferToDraw{ numBuffers, bufferSize };
+};
+
+class WaveSampleCollector
+{
+public:
+    WaveSampleCollector(WaveDrawBuffer& bufferToUse)
+        : drawBuffer(bufferToUse)
+    {
+        sampleCollecion.setSize(1, drawBuffer.getBufferSize());
+    }
+
+    void process(const float* samples, int numSamples)
+    {
+        int index = 0;
+        while (index++ < numSamples)
+        {
+            sampleCollecion.getWritePointer(0)[numCollected++] = *samples++;
+            if (numCollected == drawBuffer.getBufferSize())
+            {
+                drawBuffer.push(sampleCollecion.getReadPointer(0), sampleCollecion.getNumSamples());
+                numCollected = 0;
+            }
+        }
+    }
+
+private:
+    juce::AudioBuffer<float> sampleCollecion;
+    int numCollected{0};
+    WaveDrawBuffer& drawBuffer;
+};
+
+//==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent, public juce::Timer
 {
 public:
     //==============================================================================
@@ -25,12 +105,17 @@ public:
 
 private:
     //==============================================================================
+    virtual void timerCallback() override;
+
     float gain{ 0.5f };
     float pitch{ 1.0f };
     float lastPhase{ 0.0f };
 
     std::unique_ptr<juce::Slider> gainSlider;
     std::unique_ptr<juce::Slider> pitchSlider;
+
+    WaveDrawBuffer waveDrawBuffer;
+    WaveSampleCollector waveSampleCollector;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
