02cea2da10e2fb0485f013ccdb7f6337b863075d
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2020-11-13 02:21:52 +0900

    Change combobox justification

--- HelloSineWave/Source/MainComponent.cpp
+++ HelloSineWave/Source/MainComponent.cpp
@@ -34,6 +34,7 @@ MainComponent::MainComponent()
     
     comboboxOscillator->addItemList(oscillatorTypes, 1);
     comboboxOscillator->setSelectedItemIndex(0);
+    comboboxOscillator->setJustificationType(juce::Justification::centred);
     addAndMakeVisible(comboboxOscillator.get());
 
     startTimerHz(30);
